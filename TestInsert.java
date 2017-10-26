cfe9694982f1e6ba98339c9d77850b0de2c7213b
fenglipeng <fenglipeng598@163.com>
2017-10-26 10:49:23 +0800

    提交TestInsert文件

---
+++ TestInsert.java
@@ -0,0 +1,32 @@
+package cn.yy;
+
+public class TestInsert {
+	public static void main(String[] args) {
+		int[] arr = new int[]{91000,5,3,5,400,30,-10040};
+		System.out.print("before:");
+		print(arr);
+		System.out.print("after:");
+		sortInsert(arr);;
+		print(arr);
+	}
+	
+	public static void print(int[] arr){
+		for(int x:arr){
+			System.out.print(x+",");
+		}
+		System.out.println();
+	}
+	
+	public static void sortInsert(int[] arr){
+		int out,in;
+		for(out=1;out<arr.length;out++){
+			int temp = arr[out];
+			in = out;
+			while(in>0&&arr[in-1]>temp){
+				arr[in] = arr[in-1];
+				in --;
+			}
+			arr[in] = temp;
+		}
+	}
+}
