24c359b59066e1469640fbde9f69328205ae1548
fenglipeng <fenglipeng598@163.com>
2017-10-27 15:24:51 +0800

    devre分支上进行了文件的增加和修改

---
+++ Level.java
@@ -0,0 +1,44 @@
+package cn.mldn.em.vo;
+
+import java.io.Serializable;
+
+@SuppressWarnings("serial")
+public class Level implements Serializable {
+	private Integer lid ;
+	private Double losal ;
+	private Double hisal ;
+	private String title ;
+	private String flag ;
+	private String note ;    //devre分支上进行修改
+	public Integer getLid() {
+		return lid;
+	}
+	public void setLid(Integer lid) {
+		this.lid = lid;
+	}
+	public Double getLosal() {
+		return losal;
+	}
+	public void setLosal(Double losal) {
+		this.losal = losal;
+	}
+	public Double getHisal() {
+		return hisal;
+	}
+	public void setHisal(Double hisal) {
+		this.hisal = hisal;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getFlag() {
+		return flag;
+	}
+	public void setFlag(String flag) {
+		this.flag = flag;
+	}
+	
+}
